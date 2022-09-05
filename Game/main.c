a1e43d92c80e3a4ca2ef689a6a77a21def956bd7
xiphiasnonus <foster.pianist@gmail.com>
2022-09-05 16:11:58 -0400

    Initial Commit

---
+++ Game/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Game
+//
+//  Created by Thomas Foster on 9/5/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
